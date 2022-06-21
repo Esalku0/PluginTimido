2225bba349c2dfef687402b1953e077be936c6d0
ESTANISLAO ALIAGA KUZYK <esalku@floridauniversitaria.es>
2022-06-21 13:10:24 +0200

    Iniciando plugin y creando clase jugador

---
+++ Pyrus me hizo el lio/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exiled.API.Interfaces;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Exiled.API.Enums;
+using System.Threading.Tasks;
+
+namespace Pyrus_me_hizo_el_lio
+{
+    internal class Config : IConfig
+    {
+
+        public float EscudoBase { get; set; } = 100;
+        public float VidaBase { get; set; } = 350;
+
+        public bool IsEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Pyrus me hizo el lio/Jugador.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exiled.API.Interfaces;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Player = Exiled.API.Features.Player;
+using Exiled.API.Enums;
+using System.Threading.Tasks;
+
+
+
+namespace Pyrus_me_hizo_el_lio
+{
+    internal class Jugador : Plugin<Config>
+    {
+        public bool partidaAcabada = false;
+
+
+        public void OnPlayerVerified(VerifiedEventArgs ev)
+        {
+            Player jogador = ev.Player;
+            Zombie zomb1 = new Zombie();
+
+            if (Zombie.ContainsKey(ev.Player.UserId))
+            {
+                Z[ev.Player.UserId] = new Zombie(jogador.Id, jogador.Nickname, jogador.UserId, jogador.IPAddress);
+            }
+        }
+
+
+
+
+        //Metodo de prueba que avisa del jugador que se ha conectado al servidor
+        public void OnConnect(JoinedEventArgs ev)
+        {
+            Map.Broadcast(duration: 6, message:$"{ev.Player} has connected");
+     
+        }
+
+        //Metodo de prueba que avisa que jugador se ha desconectado                                                                                                                             
+        public void OnLeft(LeftEventArgs ev)
+        {
+            Map.Broadcast(duration: 6, message: $"{ev.Player} has Left");
+        }
+        
+        
+
+    }
+}
