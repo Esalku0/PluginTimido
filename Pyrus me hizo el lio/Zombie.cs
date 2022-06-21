2564fcda9f9038e98a9dd384fde256590b469547
ESTANISLAO ALIAGA KUZYK <esalku@floridauniversitaria.es>
2022-06-21 13:10:35 +0200

    Creando clase zombie

---
+++ Pyrus me hizo el lio/Zombie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyrus_me_hizo_el_lio
+{
+    internal class Zombie
+    {
+
+            public int PlayerId;
+            public string Name;
+            public string SteamId;
+            public bool Undead = false;
+            public bool Disconnected = false;
+
+            public Zombie(int playerId, string name, string steamId, string ipAddress)
+            {
+                this.PlayerId = playerId;
+                this.Name = name;
+                this.SteamId = steamId;
+            }
+        public Zombie()
+        {
+
+        }
+
+            public override string ToString()
+            {
+                return "[ PlayerId: " + PlayerId + ", Name: " + Name + ", SteamID: " + SteamId + ", Undead: " + Undead + ", Disconnected: " + Disconnected + " ]";
+            }
+
+
+        }
+}
