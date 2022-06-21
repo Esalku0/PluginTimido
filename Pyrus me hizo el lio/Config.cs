291c369f55157329fd943ac0348b23d6670acb12
ESTANISLAO ALIAGA KUZYK <esalku@floridauniversitaria.es>
2022-06-21 20:06:36 +0200

    Añadiendo variables de configuracion

--- Pyrus me hizo el lio/Config.cs
+++ Pyrus me hizo el lio/Config.cs
@@ -13,13 +13,15 @@ namespace Pyrus_me_hizo_el_lio
     internal class Config : IConfig
     {
         //Cantidad de escudo base que recibe el zombie al crearse
-        public float EscudoBase { get; set; } = 100;
+        public double EscudoBase { get; set; } = 100;
         //Cantidad de vida inicial del zombie
-        public float VidaBase { get; set; } = 350;
+        public double VidaBase { get; set; } = 350;
         //Booleano que guarda si el zombie se ha desconectado o no
         public bool RagueQuit { get; set; } = false;
-
-
+        //Variable que almacena el porcentaje de vida que se cura por golpe
+        public double VidaAlAtacar { get; set; } = 10;
+        //Variable que almacena el porcentaje de vida que se cura por asesinato
+        public double VidaAlAsesinar { get; set; } = 50;
 
 
 
